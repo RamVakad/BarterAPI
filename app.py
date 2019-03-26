dcb7efcb19a7341905c6427d3b2fd71c6a93064b
RamVakad <RamVakad@gmail.com>
2019-03-26 16:59:42 -0400

    Liftoff. Register, Login w/ JWT Tokens & Update Details + Profile Picture Tested & Working.

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+from api.AuthorizationAPI import auth_api
+from api.UserAPI import user_api
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+app.register_blueprint(user_api, url_prefix='/user')
+app.register_blueprint(auth_api, url_prefix='/auth')
+
+
+@app.route("/", methods=['GET'])
+def helloWorld():
+    return "Welcome to the Hunter Barterplace API. There's nothing to see here."
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, threaded=True)
