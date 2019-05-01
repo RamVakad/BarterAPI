1715aa852bd55ed1643b507a4f0a9e1f6bf114c9
bobur <uzxonboy@hotmail.com>
2019-05-01 13:05:47 -0400

    minimal search

--- api/SearchAPI.py
+++ api/SearchAPI.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, send_file, Response
 from services.DBConn import db
 import api.AuthorizationAPI
+from bson.json_util import dumps
 import json
 
 search_api = Blueprint('search_api', __name__)
@@ -15,11 +16,11 @@ def searchListings():
     query = request.args.get('query')  # /search?query=
 
     try:
-        listings = listingDB.find({'item': {'$regex': 'query'}})
+        listings = dumps(listingDB.find({'item': query}))
         if listings is None:
             return json.dumps({'error': "Searched item not found: "})
         else:
-            return json.dumps(listings)
+            return listings
     except Exception as e:
         print(e)
         return json.dumps({'error': "Server error regex searching the database.", 'code': 123})
